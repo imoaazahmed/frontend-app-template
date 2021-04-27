8e30ad3606eddb0fd9f8c421b25148c4fa5eb8f8
Moaaz <imoaazahmed@gmail.com>
2021-04-28 02:24:56 +0400

    feat: with-google-analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import * as gtag from '@lib/gtag';
+
+// CSS
 import 'tailwindcss/tailwind.css';
 import '@styles/globals.css';
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  // Google Analytics
+  useEffect(() => {
+    const handleRouteChange = (url: string) => gtag.pageview(url);
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
