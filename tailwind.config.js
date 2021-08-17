ef075da6ae46c89eed76a4f151ee59a185d187e4
Moaaz <imoaazahmed@gmail.com>
2021-08-17 16:39:22 +0400

    fix: add the screent to tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   purge: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/screens/**/*.{js,ts,jsx,tsx}',
     './src/layouts/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
