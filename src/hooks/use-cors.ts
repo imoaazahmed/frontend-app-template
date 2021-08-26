5a054eb2f34fb55572165a32d8bc7cfe61e39101
Moaaz <imoaazahmed@gmail.com>
2021-08-26 15:53:35 +0400

    refactor userAgent hook and useCors hook

--- src/hooks/use-cors.ts
+++ src/hooks/use-cors.ts
@@ -2,28 +2,40 @@ import Cors from 'cors';
 import initMiddleware from '@lib/init-middleware';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-type Methods =
+type Method =
+  | 'get'
   | 'GET'
-  | 'POST'
-  | 'PUT'
+  | 'delete'
   | 'DELETE'
+  | 'head'
   | 'HEAD'
-  | 'CONNECT'
+  | 'options'
   | 'OPTIONS'
-  | 'TRACE'
-  | 'PATCH';
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+  | 'purge'
+  | 'PURGE'
+  | 'link'
+  | 'LINK'
+  | 'unlink'
+  | 'UNLINK';
+
+const defaultMethods = ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'];
 
 export async function useCors(
   req: NextApiRequest,
   res: NextApiResponse,
-  methods?: Methods[]
+  methods?: Method[]
 ): Promise<void> {
   // Initialize the cors middleware
   const cors = initMiddleware(
     // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
     Cors({
-      // Only allow requests with GET, POST by default
-      methods: methods ? methods : ['GET', 'POST'],
+      methods: methods ? methods : defaultMethods,
     })
   );
 
