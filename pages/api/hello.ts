2ef1a58b8ef9c966712170727c9e0b4d59d1e2c2
Moaaz Ahmed <imoaazahmed@gmail.com>
2022-06-08 17:20:22 +0400

    update to react 18

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,12 @@ import { useCors } from '@hooks/use-cors';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 type TData = {
-  name: string
-}
+  name: string;
+};
 
 export default async function hello(req: NextApiRequest, res: NextApiResponse<TData>): Promise<void> {
   // Run cors
   await useCors(req, res);
 
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' });
 }
